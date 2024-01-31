0b8b1252fb3083f1a2d8531948874fc9ad4b42f7
alexIwanov <108220682+alexIwanov@users.noreply.github.com>
2024-01-31 21:23:00 +0300

    fix3

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,6 @@ import vk from './../../img/icons/vk.svg'
 import gitHub from './../../img/icons/gitHub.svg';
 import viber from './../../img/icons/viber_footer.svg';
 import telegram from './../../img/icons/telegram_footer.svg';
-import linkedin from './../../img/icons/linkedin.svg';
 import { NavLink } from 'react-router-dom';
 
 export default function Footer() {
@@ -14,10 +13,9 @@ export default function Footer() {
 				<div className="footer__wrapper">
 					<ul className="social">
 						<li className="social__item"> <NavLink to='https://vk.com/idiwanov'> <img src={vk} alt="Link" /></NavLink></li>
-						<li className="social__item"><NavLink to='https://t.me/@AlexIwanov3214'> <img src={telegram} alt="Link" /></NavLink></li>
-						<li className="social__item"><NavLink to='viber://chat?number=+375447281453'><img src={viber} alt="Link" /></NavLink></li>
+						<li className="social__item"> <img src={telegram} alt="Link" /></li>
+						<li className="social__item"><img src={viber} alt="Link" /></li>
 						<li className="social__item"><NavLink to='https://github.com/alexIwanov'><img src={gitHub} alt="Link" /></NavLink></li>
-
 					</ul>
 					<div className="copyright">
 						<p>© 2024 Frontend</p>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Header.css'
 
+
 export default function Header() {
 	return (
 		<header className="header">
@@ -12,7 +13,7 @@ export default function Header() {
 				<div className="header__text">
 					<p>with passion for learning and creating.</p>
 				</div>
-				<a href="#!" className="btn">Download CV</a>
+				<a href="https://drive.google.com/uc?export=download&confirm=no_antivirus&id=134Cwi2vRvYXVM9ZX-U9Of1y3oduz2kKM" download className="btn">Download CV</a>
 			</div>
 		</header >
 	)
